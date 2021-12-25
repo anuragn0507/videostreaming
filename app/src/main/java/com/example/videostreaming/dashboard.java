8431cc3058fd682db0552b31e12b5ec68ce1c5d9
Anurag Yadav <yadavanurag307@gmail.com>
2021-12-25 11:31:34 +0530

    video upload option

--- app/src/main/java/com/example/videostreaming/dashboard.java
+++ app/src/main/java/com/example/videostreaming/dashboard.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -17,6 +18,7 @@ public class dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         addvideo= (FloatingActionButton) findViewById(R.id.floatingActionButton6);
         addvideo.setOnClickListener(new View.OnClickListener() {
             @Override
